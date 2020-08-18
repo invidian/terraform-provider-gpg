de60c467b0ec22a8aebde400db1b7ea13da7a20f
Mateusz Gozdek <mgozdekof@gmail.com>
2020-08-18 22:18:30 +0200

    main.go: fix formatting
    Signed-off-by: Mateusz Gozdek <mgozdekof@gmail.com>

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+
 	"github.com/invidian/terraform-provider-gpg/gpg"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: gpg.Provider})
+		ProviderFunc: gpg.Provider,
+	})
 }
